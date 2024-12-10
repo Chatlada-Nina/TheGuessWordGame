ec7fc8399db8d821a84fc2fc15f3bc73487f9f2c
Chatlada Juntarawanichkul <chatladanine@gmail.com>
2024-12-10 12:01:57 +0000

    Create wordlist.js and link it with index.html file

---
+++ assets/js/wordlist.js
@@ -0,0 +1,88 @@
+// Define an array of objects wordList wih word, definition, hint and image
+
+const wordList = [
+    {
+        word: "sweetheart",
+        definition: "A person adored for their kindness, love, and charm.",
+        hint: "Someone who make life a little sweeter",
+        image: "./images/sweetheart.png"
+    },
+    {
+        word: "fireball",
+        definition: "Bright, large, moving sphere of flame or energy; explosive.",
+        hint: "Can be mesmerizing and powerful",
+        image: "./images/fireball.png"
+    },
+    {
+        word: "eyeshade",
+        definition: "Accessory worn over eyes to block light; sleeping mask.",
+        hint: "Can be a sleep game-changer",
+        image: "./images/eyeshade.png"
+    },
+    {
+        word: "household",
+        definition: "Group of people living together in a dwelling; family unit.",
+        hint: "Creating our home base",
+        image: "./images/household.png"
+    },
+    {
+        word: "hammerhead",
+        definition: "The marvel of the ocean world with flattened head shape; Hammerhead shark is a predatory fish.",
+        hint: "A predatory fish",
+        image: "./images/hammerhead.png"
+    },
+    {
+        word: "strawberry",
+        definition: "A sweet, juicy and red fruit with tiny seeds on its surface. 🍓",
+        hint: "Juicy and sweet",
+        image: "./images/strawberry.png"
+    },
+    {
+        word: "cartwheel",
+        definition: "A sideways somersault in which the body moves like a wheel with arms and legs extended. 🤸‍♂️",
+        hint: "A movement",
+        image: "./images/cartwheel.png"
+    },
+    {
+        word: "keypunch",
+        definition: "A device used to punch holes into cards for data entry and processing.",
+        hint: "Something that usually use with card or paper tape",
+        image: "./images/keypunch.png"
+    },
+    {
+        word: "carpet",
+        definition: "A carpet is a thick, woven floor covering, typically made of fibers and used for insulation.",
+        hint: "Something that we usually put on the floor",
+        image: "./images/carpet.png"
+    },
+    {
+        word: "cupcake",
+        definition: "A cupcake is a small, individual-sized cake baked in a cup-shaped container, often decorated with frosting and sprinkles.🧁",
+        hint: "A dessert for every occasion",
+        image: "./images/cupcake.png"
+    },
+    {
+        word: "brainstorm",
+        definition: "Brainstorming is a collaborative process of generating creative ideas quickly for solving problems or creating plans.",
+        hint: "Collaborate with other",
+        image: "./images/brainstorm.png"
+    },
+    {
+        word: "bodyguard",
+        definition: "A bodyguard is a trained individual who protects someone from physical harm or security threats.",
+        hint: "A protector",
+        image: "./images/bodyguard.png"
+    },
+    {
+        word: "bookworm",
+        definition: "A bookworm is a person who loves reading and devours books with great enthusiasm.",
+        hint: "A reading lover",
+        image: "./images/bookworm.png"
+    },
+    {
+        word: "waistcoat",
+        definition: "A waistcoat is a sleeveless garment worn over a shirt, typically buttoned, often part of formal attire.",
+        hint: "Cloth",
+        image: "./images/waistcoat.png"
+    }
+];
